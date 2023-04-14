fe3f9266e03224ee1dc952e42212caf77c52da48
aktam <aktam36911@gmail.com>
2023-04-14 15:25:23 +0500

    10task.dart

--- 10task.dart
+++ 10task.dart
@@ -10,4 +10,12 @@
 
 // Find the expression value assign to 'answer'. a*b / c + d .
 
-void main() {}
+void main() {
+  //write your code here
+  int a=8;
+  int b=5;
+  int c=2;
+  int d=6;
+  double answer=a*b/c+d;
+  print (answer);
+}
